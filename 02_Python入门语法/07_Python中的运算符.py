3f657180c4996d64824086f39f8f0a842c52f94f
SweetLollipop <137623637@qq.com>
2025-03-06 01:03:01 +0800

    02_Python入门语法

---
+++ 02_Python入门语法/07_Python中的运算符.py
@@ -0,0 +1,33 @@
+""""
+演示Python中的各类运算符
+"""
+
+# 算数（数学）运算符
+print("1+1=", 1+1)
+print("2-1=", 2-1)
+print("3*3=", 3*3)
+print("4/2=", 4/2)
+print("11//2=", 11//2)
+print("9%2=", 9%2)
+print("2**3=", 2**3)
+
+# 赋值运算符
+num = 1 + 2 * 3
+
+# 符合运算符
+num = 1
+num +=1
+print("num +=1", num)
+num -=1
+print("num -=1", num)
+num *=100
+print("num *=100", num)
+num /=10
+print("num /=10", num)
+num %=7
+print("num %=7", num)
+num **=2
+print("num **=2", num)
+num //=2
+print("num //=2", num)
+
